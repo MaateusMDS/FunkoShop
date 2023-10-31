f43bd0a630f3fd5c7e4bb4ba50eb4c19ece96a39
MaateusMDS <maateusmania@gmail.com>
2023-10-31 12:37:05 -0300

    fix: recursão infinita

--- src/main/java/br/com/fiap/FunkoShop/controller/LoginController.java
+++ src/main/java/br/com/fiap/FunkoShop/controller/LoginController.java
@@ -1,15 +1,16 @@
 package br.com.fiap.FunkoShop.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 public class LoginController {
 
     @GetMapping("/login")
     public ModelAndView login() {
-        ModelAndView mv = new ModelAndView("/login");
+        ModelAndView mv = new ModelAndView("login");
         return mv;
     }
 
